e92c433540d291afb6b60320f23ffccc3049ed83
Elias Mazzocco <elias.mazzocco@mytaxi.com>
2021-01-11 22:37:54 +0100

    Implement add shoe to list

--- starter/app/src/main/java/com/udacity/shoestore/screens/ShoeDetailFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/ShoeDetailFragment.kt
@@ -6,25 +6,37 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeDetailBinding
+import com.udacity.shoestore.screens.main.MainViewModel
 
 class ShoeDetailFragment : Fragment() {
 
     private lateinit var binding: FragmentShoeDetailBinding
+    private val viewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         binding = DataBindingUtil.inflate(
             inflater,
             R.layout.fragment_shoe_detail,
             container,
             false
         )
+
+        binding.buttonSave.setOnClickListener {
+            viewModel.addShoeToList(
+                name = binding.editTextName.text.toString(),
+                company = binding.editTextCompany.text.toString(),
+                size = binding.editTextSize.text.toString(),
+                description = binding.editTextDescription.text.toString()
+            )
+
+        }
 
         return binding.root
     }
