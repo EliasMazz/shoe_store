a6c720c122db5f5ea7cb710c3c9890385074cfdd
Elias Mazzocco <elias.mazzocco@mytaxi.com>
2021-01-11 21:11:24 +0100

    Map shoe to shoeViewData

--- starter/app/src/main/java/com/udacity/shoestore/screens/main/MainViewModel.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/main/MainViewModel.kt
@@ -2,13 +2,18 @@ package com.udacity.shoestore.screens.main
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.udacity.shoestore.mapper.ShoeDataMapper
 import com.udacity.shoestore.models.Shoe
+import com.udacity.shoestore.models.ShoeDataView
 
 class MainViewModel : ViewModel() {
     private val _shoeListViewData = MutableLiveData<List<Shoe>>()
-    val shoeListViewData: LiveData<List<Shoe>>
-        get() = _shoeListViewData
+    val shoeListViewData: LiveData<List<ShoeDataView>>
+        get() = Transformations.map(_shoeListViewData) {
+            it.map { shoe -> ShoeDataMapper.mapToViewData(shoe) }
+        }
 
     init {
         _shoeListViewData.value = mutableListOf(
