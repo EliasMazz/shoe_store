fc21e05bc4b50d09a045fb2b939c817f5a769e7d
Elias Mazzocco <elias.mazzocco@mytaxi.com>
2021-01-12 00:33:48 +0100

    Setup acttion bar with nav controller

--- starter/app/src/main/java/com/udacity/shoestore/screens/main/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/main/MainActivity.kt
@@ -3,18 +3,29 @@ package com.udacity.shoestore.screens.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.udacity.shoestore.R
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var viewModel: MainViewModel
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
 
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onSupportNavigateUp() = NavigationUI.navigateUp(navController, appBarConfiguration)
 }
