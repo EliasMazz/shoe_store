eeb1e0f33ec79d674faaeff2ca1e453d0aea47f0
Elias Mazzocco <elias.mazzocco@mytaxi.com>
2021-01-13 22:03:13 +0100

    Refactor viewModel to support two way data binding

--- starter/app/src/main/java/com/udacity/shoestore/screens/main/MainViewModel.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/main/MainViewModel.kt
@@ -9,27 +9,36 @@ import com.udacity.shoestore.models.Shoe
 import com.udacity.shoestore.models.ShoeDataView
 
 class MainViewModel : ViewModel() {
+
+    var shoeDataView = ShoeDataView()
     private val _shoeListViewData = MutableLiveData<MutableList<Shoe>>()
     val shoeListViewData: LiveData<List<ShoeDataView>>
         get() = Transformations.map(_shoeListViewData) {
             it.map { shoe -> ShoeDataMapper.mapToViewData(shoe) }
         }
 
+    private val _eventNavigate = MutableLiveData<Boolean>()
+    val eventNavigate: LiveData<Boolean>
+        get() = _eventNavigate
+
     init {
         _shoeListViewData.value = mutableListOf()
+        _eventNavigate.value = false
     }
 
-    fun addShoeToList(
-        name: String,
-        company: String,
-        size: String,
-        description: String
-    ) = _shoeListViewData.value?.add(
-        ShoeDataMapper.mapFromViewData(
-            name = name,
-            company = company,
-            size = size,
-            description = description
+    fun onSave() {
+        _shoeListViewData.value?.add(
+            ShoeDataMapper.mapFromViewData(shoeDataView)
         )
-    )
+        shoeDataView = ShoeDataView()
+        _eventNavigate.value = true
+    }
+
+    fun onCancel() {
+        _eventNavigate.value = true
+    }
+
+    fun onNavigateComplete() {
+        _eventNavigate.value = false
+    }
 }
