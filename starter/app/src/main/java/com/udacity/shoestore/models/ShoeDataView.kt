df9673bf3059fe6828866328cc90b8e5474aaa42
Elias Mazzocco <elias.mazzocco@mytaxi.com>
2021-01-13 22:03:59 +0100

    Add default values to ShoeDataView

--- starter/app/src/main/java/com/udacity/shoestore/models/ShoeDataView.kt
+++ starter/app/src/main/java/com/udacity/shoestore/models/ShoeDataView.kt
@@ -1,3 +1,8 @@
 package com.udacity.shoestore.models
 
-data class ShoeDataView(var name: String, var size: String, var company: String, var description: String)
+data class ShoeDataView(
+    var name: String = "",
+    var size: String = "",
+    var company: String = "",
+    var description: String = ""
+)
